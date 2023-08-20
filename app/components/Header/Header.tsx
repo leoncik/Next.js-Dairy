b79086a86dfb66c886bdfb519c08dfbaf8758b9b
Léonard Wojcik <leonardw@tutanota.com>
2023-08-20 11:08:38 +0200

    feat: create header

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles["nav-links"]}>
+          <li>
+            <Link href="#">Home</Link>
+          </li>
+          <li>
+            <Link href="#">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
