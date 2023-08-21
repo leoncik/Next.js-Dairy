96f660101428bcccb665f08a8420d65f029dd748
Léonard Wojcik <leonardw@tutanota.com>
2023-08-21 15:50:20 +0200

    feat: improve styling of post cards

--- app/components/PostCard/PostCard.tsx
+++ app/components/PostCard/PostCard.tsx
@@ -1,5 +1,6 @@
 import getFormattedDate from "@/lib/getFormattedDate";
 import Link from "next/link";
+import styles from "./PostCard.module.css";
 
 type PostItemProps = {
   dairyPost: DairyPost;
@@ -8,10 +9,12 @@ type PostItemProps = {
 export default function PostItem({ dairyPost }: PostItemProps) {
   const formattedDate = getFormattedDate(dairyPost.date);
   return (
-    <li>
-      <Link href={`/posts/${dairyPost.id}`}>{dairyPost.title}</Link>
-      <br />
-      <p>{formattedDate}</p>
+    <li className={styles.card}>
+      <Link className={styles.link} href={`/posts/${dairyPost.id}`}>
+        <h3>{dairyPost.title}</h3>
+        <br />
+        <p>{formattedDate}</p>
+      </Link>
     </li>
   );
 }
