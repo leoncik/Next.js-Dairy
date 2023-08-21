fd3dbd5a7b6eba86ce43fb79b27d5c3a637a0d4b
Léonard Wojcik <leonardw@tutanota.com>
2023-08-21 16:51:10 +0200

    feat: add style to the about page

---
+++ app/about/AboutArmy.tsx
@@ -0,0 +1,57 @@
+import styles from "./AboutArmy.module.css";
+
+export default function AboutArmy() {
+  return (
+    <div className={styles.background}>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+      <span>About</span>
+    </div>
+  );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,18 @@
+import AboutArmy from "./AboutArmy";
+import styles from "./page.module.css";
+
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is all about dairies!</p>
-    </div>
+    <>
+      <div className={styles.about}>
+        <h1 className={styles.title}>That&apos;s what I call an about page!</h1>
+        <div>
+          <p>About</p>
+        </div>
+      </div>
+      <div>
+        <AboutArmy />
+      </div>
+    </>
   );
 }
