156e822ae4fb37999e32792001340d081fedf860
Léonard Wojcik <leonardw@tutanota.com>
2023-08-21 15:58:49 +0200

    feat: add style to post detail

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getPostData, getSortedDairyPostsData } from "@/lib/dairyPosts";
 import getFormattedDate from "@/lib/getFormattedDate";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import styles from "./page.module.css";
 
 export default async function PostDetail({
   params,
@@ -17,8 +18,9 @@ export default async function PostDetail({
   const { title, date, contentHtml } = await getPostData(params.id);
   const formattedDate = getFormattedDate(date);
   return (
-    <article>
+    <article className={styles["content-wrapper"]}>
       <h2>{title}</h2>
+      <span>{formattedDate}</span>
       <section dangerouslySetInnerHTML={{ __html: contentHtml }}></section>
       <Link href="/">Back to home page</Link>
     </article>
